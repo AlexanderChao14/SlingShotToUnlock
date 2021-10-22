3d7a33c28111ecdefc3d9aa33b03c94e0cee79c0
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2021-10-21 18:24:09 -0600

    kinda got sling shot to work

--- sling-shot-to-unlock/src/components/PlayZone.js
+++ sling-shot-to-unlock/src/components/PlayZone.js
@@ -1,11 +1,52 @@
 import Slingshot from "./Slingshot"
+import {useState} from 'react'
 
 const PlayZone = () => {
+    const [isMouseDown, setIsMouseDown] = useState(false)
+    const [mouseX, setMouseX] = useState(null)
+    const [mouseY, setMouseY] = useState(null)
+    
+
+
+    function mouseDown(e){
+        e.preventDefault();
+        setIsMouseDown(true)
+        console.log('mouse down')
+    }
+
+    function mouseRelease(e){
+        e.preventDefault();
+        setIsMouseDown(false)
+        console.log('mouse released')
+    }
+
+    function mouseMove(e){
+        if(isMouseDown){
+            relativeCoords(e);
+        }
+    }
+
+    function drawArrowPoint(){
+        return(
+            <div className="arrow-point" style={{right: mouseX - 5+'px', bottom: mouseY + 5+'px'}}></div>
+        );
+    }
+
+    function relativeCoords ( event ) {
+        var bounds = event.target.getBoundingClientRect();
+        console.log(bounds)
+        setMouseX(event.clientX - bounds.left);
+        setMouseY(event.clientY - bounds.top);
+    }
+
     return (
         <div className="PlayZone">
-            <div className = "PlayZone_Content">
-                <Slingshot/>
+            <div className = "PlayZone_Content"  onMouseUp={mouseRelease} onMouseMove={mouseMove}>
+                <Slingshot onMouseDownFunc={mouseDown}>
+                </Slingshot>
+                {(isMouseDown ? drawArrowPoint() : <div></div>) }
             </div>
+            
         </div>
     )
 }
