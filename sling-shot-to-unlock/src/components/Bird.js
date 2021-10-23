65097eb2abc09d5645519b83122a3344235888cd
AlexanderChao14 <chaoalexander45@gmail.com>
2021-10-22 19:46:29 -0600

    Fix the bird position issue

--- sling-shot-to-unlock/src/components/Bird.js
+++ sling-shot-to-unlock/src/components/Bird.js
@@ -7,16 +7,16 @@ const Bird = ({id}) => {
     var bird_src;
 
     switch(id) {
-        case "1":
+        case 1:
             bird_src = red_bird;
             break;
-        case "2":
+        case 2:
             bird_src = blue_bird;
             break;
-        case "3":
+        case 3:
             bird_src = yellow_bird;
             break;
-        case "4":
+        case 4:
             bird_src = green_bird;
             break;
         default:
@@ -25,7 +25,7 @@ const Bird = ({id}) => {
 
     return (
         <div>
-            <img src={bird_src} id="1" alt="red-bird" className="currentBird"></img>
+            <img src={bird_src} alt="red-bird" className="currentBird"></img>
         </div>
 
     );
