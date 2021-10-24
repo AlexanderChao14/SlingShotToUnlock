83a4f81fe401f6951ff233cb20904e333e0d19ec
AlexanderChao14 <chaoalexander45@gmail.com>
2021-10-24 01:35:07 -0600

    Added Rotate attempt 1 not tested with main branch

--- sling-shot-to-unlock/src/components/Slingshot.js
+++ sling-shot-to-unlock/src/components/Slingshot.js
@@ -1,12 +1,13 @@
-
+import SlingShotImg from "../Assets/slingshot_frame_0001.png";
 
 
 
 
 const Slingshot = ({onMouseDownFunc}) =>{
-
+    
     return(
         <div className="SlingShot" onMouseDown={onMouseDownFunc}>
+            <img src={SlingShotImg} className="SlingShot2"></img>
         </div>
     )
 }
